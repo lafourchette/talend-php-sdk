1ebfa012633591aada787d662b67bf3b3ad1966a
Romain Delouard <rdelouard@lafourchette.com>
2014-11-13 16:03:06 +0100

    [crm-197] Create client talend api

---
+++ test/LaFourchette/Talend/TalendClientTest.php
@@ -0,0 +1,120 @@
+<?php
+namespace test\LaFourchette\Talend;
+
+use LaFourchette\Talend\TalendClient;
+use Guzzle\Http\Message\Request;
+use Guzzle\Http\Message\Response;
+use Guzzle\Plugin\Mock\MockPlugin;
+use Guzzle\Tests\GuzzleTestCase;
+
+/**
+ * Class TalendClientTest
+ *
+ * @package LaFourchette\Talend
+ */
+class TalendClientTest extends GuzzleTestCase
+{
+    /**
+     * @var MockPlugin
+     */
+    private $mock;
+
+    /**
+     * @var TalendClient
+     */
+    private $client;
+
+    /**
+     * setUp
+     */
+    public function setUp()
+    {
+        $this->mock = new MockPlugin;
+        $this->client = TalendClient::factory(array(
+            'base_url'    => 'http://talend.url/org.talend.administrator/metaServlet',
+            'login'       => 'login',
+            'password'    => 'password',
+        ));
+        $this->client->addSubscriber($this->mock);
+    }
+
+    /**
+     * method : runTask
+     * case :  run the task with id=17 (job_label)
+     */
+    public function testRunTask()
+    {
+
+        $this->mock->addResponse(new Response(
+            200,
+            array(
+                'Content-Type' => 'application/json',
+            ),
+            <<<BODY
+{
+    "returnCode": 0
+}
+BODY
+        ));
+        $response = $this->client->runTask(17);
+
+        $requests = $this->mock->getReceivedRequests();
+
+        $this->assertCount(1, $requests);
+        $request = reset($requests);
+
+        $return = json_decode($response->getBody(true));
+        $this->assertEquals(0, $return->returnCode);
+        $this->assertEquals(
+            'http://talend.url/org.talend.administrator/metaServlet?eyJhY3Rpb25OYW1lIjoicnVuVGFzayIsImF1dGhQYXNzIjoicGFzc3dvcmQiLCJhdXRoVXNlciI6ImxvZ2luIiwidGFza0lkIjoxNywibW9kZSI6ImFzeW5jaHJvbm91cyJ9',
+            $request->getUrl()
+        );
+    }
+
+    /**
+     * method : listTasks
+     * case : search a task with a label job_label
+     */
+    public function testListTasks()
+    {
+
+        $this->mock->addResponse(new Response(
+            200,
+            array(
+                'Content-Type' => 'application/json',
+            ),
+            <<<BODY
+{
+    "result": [{
+        "label": "job_label"
+    }],
+    "returnCode": 0
+}
+BODY
+        ));
+
+        $response = $this->client->listTasks();
+        $return = json_decode($response->getBody(true));
+
+        $requests = $this->mock->getReceivedRequests();
+
+        $this->assertCount(1, $requests);
+        $request = reset($requests);
+
+        $this->assertEquals(0, $return->returnCode);
+
+        $this->assertEquals(
+            'http://talend.url/org.talend.administrator/metaServlet?eyJhY3Rpb25OYW1lIjoibGlzdFRhc2tzIiwiYXV0aFBhc3MiOiJwYXNzd29yZCIsImF1dGhVc2VyIjoibG9naW4iLCJtb2RlIjoic3luY2hyb25vdXMifQ=%3D',
+            $request->getUrl()
+        );
+
+        $taskLabels = array();
+        if (!empty($return->result)) {
+            foreach ($return->result as $task) {
+                $taskLabels[] = $task->label;
+            }
+        }
+
+        $this->assertTrue(array_search('job_label', $taskLabels) !== false);
+    }
+}
